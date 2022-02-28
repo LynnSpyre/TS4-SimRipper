8bee363401b4803672f67408274cbabc949e0df7
thepancake1 <theojabalde@gmail.com>
2022-02-28 14:17:48 +0800

    include multiprocess

--- multiprocess_run.bat
+++ multiprocess_run.bat
@@ -3,6 +3,7 @@
 echo %time%
 
 (
+start "" multiprocess_runsingle.bat TS4SimRipper.exe 1 0 "E:\Dropbox\Machine Learning\Sims 4 Saves\5k YA Sims Set 1.save"
 start "" multiprocess_runsingle.bat TS4SimRipper.exe 1 0 "E:\Dropbox\Machine Learning\Sims 4 Saves\5k YA Sims Set 2.save"
 start "" multiprocess_runsingle.bat TS4SimRipper.exe 1 0 "E:\Dropbox\Machine Learning\Sims 4 Saves\5k YA Sims Set 3.save"
 start "" multiprocess_runsingle.bat TS4SimRipper.exe 1 0 "E:\Dropbox\Machine Learning\Sims 4 Saves\5k YA Sims Set 4.save"
